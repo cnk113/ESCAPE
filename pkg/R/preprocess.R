cc17635d907e651b8e9fb3b1de62f7bdd92cd13d
Chang <dfrontier1@gmail.com>
2019-11-06 15:27:09 -0800

    split pipeline into two steps

---
+++ pkg/R/preprocess.R
@@ -0,0 +1,66 @@
+# Run CellBender
+CellBender = function(h5, cells, total, calls, dim=200, layers=1000, epochs=300){
+  command = paste("cellbender remove-background --input", eval(h5),
+                 "--output output.h5",
+                 "--cuda",
+                 "--expected_cells", eval(cells),
+                 "--total-droplets-included", eval(total),
+                 "--z-dim", eval(dim)
+                 "--z-layers", eval(layers)
+                 "--epochs", eval(epochs))
+  count <- Read10X_h5('output.h5')
+  count
+}
+
+
+# Run doublet removals tools
+preprocess = function(h5, exp.cells, total.cells, cell.calls, z.dim, z.layers, epochs){
+  count <- CellBender(h5, cells=exp.cells, total=total.cells, calls=cell.calls, dim=z.dim, layers=z.layers, epochs=epochs)
+  source('doubletdetect.py')
+  pred = doubletdetect(count, exp.cells, cell.calls)
+  DoubletDecon(count)
+  DoubletFinder(count, prop)
+  consensus
+  count[,consensus]
+}
+
+
+# Run DoubletFinder
+DoubletFinder = function(count, prop=0.1){
+  seuratObject = CreateSeuratObject(count)
+  seuratObject = SCTransform(seuratObject)
+  seuratObject = RunPCA(seuratObject)
+  seuratObject = RunUMAP(seuratObject, dims=1:30)
+  sweep = paramSweep_v3(seuratObject, PCs=1:30, sct=F)
+  sweep.stats = summarizeSweep(sweep, GT=F)
+  sweep.pk = find.pK(sweep.stats)
+  homotypic = modelHomotypic(seuratObject@meta.data$seurat.clusters)
+  nExp_poi <- round(prop*length(colnames(seuratObject)))
+  nExp_poi.adj <- round(nExp_poi*(1-homotypic.prop))
+  seuratObject <- doubletFinder_v3(seuratObject, PCs = 1:30, pN = 0.25, pK = 0.09, nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
+  seuratObject <- doubletFinder_v3(seuratObject, PCs = 1:30, pN = 0.25, pK = 0.09, nExp = nExp_poi.adj, reuse.pANN = "pANN_0.25_0.09_913", sct = FALSE)
+}
+
+
+
+# Run DoubletDecon
+DoubletDecon = function(count){
+  seuratObject = CreateSeuratObject(count)
+  newFiles = Improved_Seurat_Pre_Process(seuratObject, num_genes=50, write_files=FALSE)
+  results = Main_Doublet_Decon(rawDataFile=newFiles$newExpressionFile,
+                             groupsFile=newFiles$newGroupsFile,
+                             filename=filename,
+                             location=location,
+                             fullDataFile=NULL,
+                             removeCC=FALSE,
+                             species="hsa",
+                             rhop=1.1,
+                             write=TRUE,
+                             PMF=TRUE,
+                             useFull=FALSE,
+                             heatmap=FALSE,
+                             centroids=TRUE,
+                             num_doubs=100,
+                             only50=FALSE,
+                             min_uniq=4)
+
